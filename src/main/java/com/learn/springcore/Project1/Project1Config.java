59fe07039abd96d402e574a34859a30222d53e98
unknown <chogalesanika8@gmail.com>
2024-11-08 19:38:06 +0530

    initial

--- src/main/java/com/learn/springcore/Project1/Project1Config.java
+++ src/main/java/com/learn/springcore/Project1/Project1Config.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Project1Config {
 
+	public Courses courses()
+	{
+		return new Courses();
+	}
+	
 	public Institute institute1()
 	{
 		return new Institute(); 
