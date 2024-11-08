182f1e6f972cbae811028fb66022d183554ae4d5
unknown <chogalesanika8@gmail.com>
2024-11-08 19:29:18 +0530

    initial

--- src/main/java/com/learn/springcore/Project1/Courses.java
+++ src/main/java/com/learn/springcore/Project1/Courses.java
@@ -6,4 +6,23 @@ public class Courses {
 	private String CourseName;
 	private int CoursePrice;
 	
+	public int getCourseId() {
+		return CourseId;
+	}
+	public void setCourseId(int courseId) {
+		CourseId = courseId;
+	}
+	public String getCourseName() {
+		return CourseName;
+	}
+	public void setCourseName(String courseName) {
+		CourseName = courseName;
+	}
+	public int getCoursePrice() {
+		return CoursePrice;
+	}
+	public void setCoursePrice(int coursePrice) {
+		CoursePrice = coursePrice;
+	}
+	
 }
