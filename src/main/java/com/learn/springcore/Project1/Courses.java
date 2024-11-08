21f965b4db7de73471a2c0640c54dcb62c5bc623
unknown <chogalesanika8@gmail.com>
2024-11-08 19:25:26 +0530

    initial

---
+++ src/main/java/com/learn/springcore/Project1/Courses.java
@@ -0,0 +1,5 @@
+package com.learn.springcore.Project1;
+
+public class Courses {
+
+}
---
+++ src/main/java/com/learn/springcore/Project1/Institute.java
@@ -0,0 +1,29 @@
+package com.learn.springcore.Project1;
+
+public class Institute {
+
+	private int InstituteId;
+	private String InstituteName;
+	private int InstitudeAddress;
+	
+	public int getInstituteId() {
+		return InstituteId;
+	}
+	public void setInstituteId(int instituteId) {
+		InstituteId = instituteId;
+	}
+	public String getInstituteName() {
+		return InstituteName;
+	}
+	public void setInstituteName(String instituteName) {
+		InstituteName = instituteName;
+	}
+	public int getInstitudeAddress() {
+		return InstitudeAddress;
+	}
+	public void setInstitudeAddress(int institudeAddress) {
+		InstitudeAddress = institudeAddress;
+	}
+	
+	
+}
